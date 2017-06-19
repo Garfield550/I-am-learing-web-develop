e920bdf4b6f1f76b7fef030a3fe3c31a53ec3513
Garfield550 <garfield_550@outlook.com>
2017-06-19 16:29:18 +0800

    Add bhHeader and change font-family

--- BeiHaiGuzhengClub/src/App.vue
+++ BeiHaiGuzhengClub/src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <bh-header></bh-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import bhHeader from '@/components/Header';
+
 export default {
   name: 'app',
+  components: {
+    bhHeader,
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, STSong, NSimSun, SimSun, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #666666;
 }
 </style>
