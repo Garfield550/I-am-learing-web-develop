e1984b97a470a202a9c88a0888430d5778ee7058
Garfield550 <garfield_550@outlook.com>
2017-06-20 04:22:12 +0800

    Add routers

--- BeiHaiGuzhengClub/src/router/index.js
+++ BeiHaiGuzhengClub/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import bhIndex from '@/components/Index';
+import bhContact from '@/components/Contact';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: bhIndex,
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: bhContact,
     },
   ],
 });
