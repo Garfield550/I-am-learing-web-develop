986b633f4ea6fed70a0c554680e8c69bc74b5186
Garfield550 <garfield_550@outlook.com>
2017-06-24 17:49:24 +0800

    index.js: add intro router

--- BeiHaiGuzhengClub/Mobile/src/router/index.js
+++ BeiHaiGuzhengClub/Mobile/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import bhIndex from '@/components/Index';
+import bhIntro from '@/components/Intro';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: bhIndex,
+    },
+    {
+      path: '/intro',
+      name: 'Intro',
+      component: bhIntro,
     },
   ],
 });
