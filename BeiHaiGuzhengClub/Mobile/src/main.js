367104ff76fdd1c48c830a8c4d145f7eddb56089
Garfield550 <garfield_550@outlook.com>
2017-06-24 16:49:29 +0800

    main.js: add vue-awesome-swiper

--- BeiHaiGuzhengClub/Mobile/src/main.js
+++ BeiHaiGuzhengClub/Mobile/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
 import App from './App';
 import router from './router';
 
 Vue.use(Vuetify);
+Vue.use(VueAwesomeSwiper);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
