ef527e20e3dae064dea515f9a375e6898bc22ff8
Garfield550 <garfield_550@outlook.com>
2017-06-25 14:05:35 +0800

    main.js: add vue-amap and use vue-amap

--- BeiHaiGuzhengClub/Mobile/src/main.js
+++ BeiHaiGuzhengClub/Mobile/src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import VueAwesomeSwiper from 'vue-awesome-swiper';
+import AMap from 'vue-amap';
 import App from './App';
 import router from './router';
 
 Vue.use(Vuetify);
 Vue.use(VueAwesomeSwiper);
+Vue.use(AMap);
+AMap.initAMapApiLoader({
+  key: 'aef78700d74611fe8f4a2f3c787c53bc',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+});
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
